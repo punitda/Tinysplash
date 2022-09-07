761af2b7362ad7168253ddd1141ee9db5e4c9af7
punitda <punitdama@gmail.com>
2022-09-07 13:15:52 +0530

    Integrate PhotoListScreenRoute in MainActivity

--- app/src/main/java/dev/punitd/unplashapp/MainActivity.kt
+++ app/src/main/java/dev/punitd/unplashapp/MainActivity.kt
@@ -1,43 +1,50 @@
+@file:OptIn(ExperimentalCoilApi::class)
+
 package dev.punitd.unplashapp
 
 import android.os.Bundle
+import android.widget.Toolbar
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import coil.annotation.ExperimentalCoilApi
+import dagger.hilt.android.AndroidEntryPoint
+import dev.punitd.unplashapp.screen.photos.PhotosListScreen
+import dev.punitd.unplashapp.screen.photos.PhotosListScreenRoute
 import dev.punitd.unplashapp.ui.theme.MoleculeUnplashAppTheme
 
+@AndroidEntryPoint
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MoleculeUnplashAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                Scaffold(
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            title = { Text("Home") },
+                            actions = {
+                                IconButton(onClick = {}) {
+                                    Icon(
+                                        imageVector = Icons.Default.Search,
+                                        contentDescription = "Search Icon"
+                                    )
+                                }
+                            }
+                        )
+                    },
+                    content = {
+                        PhotosListScreenRoute(
+                            modifier = Modifier.padding(it)
+                        )
+                    }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MoleculeUnplashAppTheme {
-        Greeting("Android")
     }
 }
