86d6f3d2a4f75ac1b87f0770b2f47fd3dc9f1ae8
punitda <punitdama@gmail.com>
2022-09-07 12:53:50 +0530

    Add new annotation

--- app/src/main/java/dev/punitd/unplashapp/di/Annotations.kt
+++ app/src/main/java/dev/punitd/unplashapp/di/Annotations.kt
@@ -5,3 +5,7 @@ import javax.inject.Qualifier
 @Qualifier
 @Retention(AnnotationRetention.RUNTIME)
 annotation class UnsplashApiKey
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class IODispatcher
