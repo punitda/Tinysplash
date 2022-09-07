0251a49f90257b5055a9f63a9ce7f613296ceedf
punitda <punitdama@gmail.com>
2022-09-07 13:14:59 +0530

    Create PhotosListViewModel

---
+++ app/src/main/java/dev/punitd/unplashapp/screen/photos/PhotosListViewModel.kt
@@ -0,0 +1,106 @@
+package dev.punitd.unplashapp.screen.photos
+
+import androidx.compose.runtime.*
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import app.cash.molecule.AndroidUiDispatcher
+import app.cash.molecule.RecompositionClock
+import app.cash.molecule.launchMolecule
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.punitd.unplashapp.data.UnsplashRepository
+import dev.punitd.unplashapp.model.Error
+import dev.punitd.unplashapp.model.PageLinks
+import dev.punitd.unplashapp.model.Success
+import dev.punitd.unplashapp.model.UnsplashImage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class PhotosListViewModel @Inject constructor(
+    private val unsplashRepository: UnsplashRepository
+) : ViewModel() {
+    private val scope = CoroutineScope(viewModelScope.coroutineContext + AndroidUiDispatcher.Main)
+    private val events = MutableSharedFlow<Event>()
+    val stateFlow = scope.launchMolecule(clock = RecompositionClock.ContextClock) {
+        present(events)
+    }
+
+
+    init {
+        processEvent(InitialPageEvent)
+    }
+
+    @Composable
+    private fun present(events: Flow<Event>): PhotosListUIState {
+        var isLoading by remember { mutableStateOf(false) }
+        var error by remember { mutableStateOf<String?>(null) }
+        var isPaginationLoading by remember { mutableStateOf(false) }
+        var paginationError by remember { mutableStateOf<String?>(null) }
+        val images = remember { mutableStateListOf<UnsplashImage>() }
+        var pageLinks by remember { mutableStateOf<PageLinks?>(null) }
+
+        LaunchedEffect(Unit) {
+            events.collect { event ->
+                when (event) {
+                    InitialPageEvent -> {
+                        isLoading = true
+                        when (val result = unsplashRepository.getPhotos(page = 1, perPage = 10)) {
+                            is Error -> {
+                                isLoading = false
+                                error = result.message
+                            }
+                            is Success -> {
+                                isLoading = false
+                                images.addAll(result.data.images)
+                                pageLinks = result.data.pageLinks
+                            }
+                        }
+
+                    }
+                    is PaginateEvent -> {
+                        if (isPaginationLoading) return@collect
+                        val nextPageUrl = pageLinks?.next ?: return@collect
+                        isPaginationLoading = true
+                        when (val result = unsplashRepository.getPhotosByUrl(nextPageUrl)) {
+                            is Error -> {
+                                isPaginationLoading = false
+                                paginationError = result.message
+                            }
+                            is Success -> {
+                                isPaginationLoading = false
+                                images.addAll(result.data.images)
+                                pageLinks = result.data.pageLinks
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return PhotosListUIState(
+            isLoading = isLoading,
+            error = error,
+            images = images,
+            pageLinks = pageLinks,
+            isPaginationLoading = isPaginationLoading,
+            paginationError = paginationError,
+        )
+    }
+
+    fun processEvent(event: Event) {
+        scope.launch {
+            events.emit(event)
+        }
+    }
+}
+
+data class PhotosListUIState(
+    val isLoading: Boolean = false,
+    val isPaginationLoading: Boolean = false,
+    val error: String? = null,
+    val paginationError: String? = null,
+    val images: List<UnsplashImage> = emptyList(),
+    val pageLinks: PageLinks? = null,
+)
