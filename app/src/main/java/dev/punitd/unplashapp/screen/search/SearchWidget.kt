601b7763bc333af8aedbbca6c769193bf6349bfe
punitda <punitdama@gmail.com>
2022-09-08 16:00:42 +0530

    Create SearchWidget

---
+++ app/src/main/java/dev/punitd/unplashapp/screen/search/SearchWidget.kt
@@ -0,0 +1,79 @@
+package dev.punitd.unplashapp.screen.search
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.DefaultAlpha
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchWidget(
+    isSuggestionLoading: Boolean,
+    text: String,
+    onTextChange: (String) -> Unit,
+    onClearClicked: () -> Unit,
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp)
+            .semantics {
+                contentDescription = "SearchWidget"
+            },
+        shadowElevation = 8.dp,
+        color = MaterialTheme.colorScheme.primary,
+    ) {
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .semantics {
+                    contentDescription = "TextField"
+                },
+            value = text,
+            onValueChange = onTextChange,
+            placeholder = {
+                Text(
+                    modifier = Modifier.alpha(alpha = DefaultAlpha),
+                    text = "Search here..",
+                    color = Color.White
+                )
+            },
+            textStyle = TextStyle(
+                color = MaterialTheme.colorScheme.onBackground
+            ),
+            singleLine = true,
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search Icon"
+                )
+            },
+            trailingIcon = {
+                if (isSuggestionLoading) {
+                    CircularProgressIndicator(Modifier.size(24.dp))
+                } else if (text.isNotBlank()) {
+                    IconButton(
+                        onClick = onClearClicked
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = "Close Icon"
+                        )
+                    }
+                }
+            }
+        )
+    }
+}
