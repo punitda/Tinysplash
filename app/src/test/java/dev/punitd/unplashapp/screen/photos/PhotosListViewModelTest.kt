92d5fa7ce5ad8050801b95de6114be43c4647142
punitda <punitdama@gmail.com>
2022-09-14 16:14:01 +0530

    Update test

--- app/src/test/java/dev/punitd/unplashapp/screen/photos/PhotosListViewModelTest.kt
+++ app/src/test/java/dev/punitd/unplashapp/screen/photos/PhotosListViewModelTest.kt
@@ -1,8 +1,11 @@
 package dev.punitd.unplashapp.screen.photos
 
 import app.cash.molecule.RecompositionClock
+import app.cash.turbine.test
 import dev.punitd.unplashapp.data.fake.FakeUnsplashRepository
+import dev.punitd.unplashapp.model.images
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -12,22 +15,24 @@ import org.junit.Test
 class PhotosListViewModelTest {
 
     @Test
-    fun initialState() = runTest {
-
-        // Arrange
+    fun successStateIfApiSucceeds() = runTest {
         val viewModel = PhotosListViewModel(
             unsplashRepository = FakeUnsplashRepository(),
-            scope = this,
+            scope = this, // TestScope
             clock = RecompositionClock.Immediate,
         )
 
+        // InitialPageEvent is send inside VM's init{} block by default
+        // That's why we're not sending any events here.
 
-        val expectedUIState = PhotosListUIState(
-            isLoading = false,
-            error = null,
-            images = listOf(),
-        )
-
-        assertEquals(expectedUIState, viewModel.stateFlow.value)
+        viewModel.stateFlow.test {
+            assertEquals(PhotosListUIState(isLoading = false), awaitItem())
+            assertEquals(PhotosListUIState(isLoading = true), awaitItem())
+            runCurrent()
+            assertEquals(
+                PhotosListUIState(isLoading = false, error = null, images = images),
+                awaitItem()
+            )
+        }
     }
 }
