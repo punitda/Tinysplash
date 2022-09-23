c8afc3c395e70c12e2bb9a31c02468b417a9a7e3
punitda <punitdama@gmail.com>
2022-09-23 15:10:11 +0530

    Create Semantics and Tags

---
+++ app/src/main/java/dev/punitd/Semantics.kt
@@ -0,0 +1,8 @@
+package dev.punitd
+
+object Semantics {
+
+    const val RetryAction = "retry_action"
+    const val SearchInput = "search_input"
+    const val ClearInput = "clear_input"
+}
---
+++ app/src/main/java/dev/punitd/Tags.kt
@@ -0,0 +1,8 @@
+package dev.punitd
+
+object Tags {
+    const val ScreenLoader = "screen_loader"
+    const val ImagesList = "images_list"
+    const val PaginationLoader = "pagination_loader"
+    const val SearchSuggestionsLoader = "search_suggestions_loader"
+}
