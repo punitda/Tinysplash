0fde8de1b3516e73cfb2c98cf5e9347d850ac55e
punitda <punitdama@gmail.com>
2022-09-25 15:56:21 +0530

    Add testTag to BottomBar destinations

--- app/src/main/java/dev/punitd/unplashapp/components/BottomBar.kt
+++ app/src/main/java/dev/punitd/unplashapp/components/BottomBar.kt
@@ -6,13 +6,16 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import com.ramcosta.composedestinations.navigation.navigate
 import com.ramcosta.composedestinations.navigation.popBackStack
 import com.ramcosta.composedestinations.navigation.popUpTo
 import com.ramcosta.composedestinations.spec.DirectionDestinationSpec
+import dev.punitd.Tags
 import dev.punitd.unplashapp.R
 import dev.punitd.unplashapp.screen.NavGraphs
 import dev.punitd.unplashapp.screen.appCurrentDestinationAsState
@@ -32,6 +35,7 @@ fun BottomBar(
     NavigationBar {
         BottomBarDestination.values().forEach { destination ->
             NavigationBarItem(
+                modifier = Modifier.testTag(destination.testTag),
                 selected = currentDestination == destination.direction,
                 onClick = {
                     if (currentDestination == destination.direction) {
@@ -69,7 +73,18 @@ enum class BottomBarDestination(
     val direction: DirectionDestinationSpec,
     val icon: ImageVector,
     @StringRes val label: Int,
+    val testTag: String,
 ) {
-    Photos(PhotosListScreenRouteDestination, Icons.Default.Home, R.string.home_screen),
-    Search(SearchPhotosScreenRouteDestination, Icons.Default.Search, R.string.search_screen)
+    Photos(
+        PhotosListScreenRouteDestination,
+        Icons.Default.Home,
+        R.string.home_screen,
+        Tags.PhotosTab
+    ),
+    Search(
+        SearchPhotosScreenRouteDestination,
+        Icons.Default.Search,
+        R.string.search_screen,
+        Tags.SearchTab
+    )
 }
